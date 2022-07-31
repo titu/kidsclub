53d367dcf726bc8b4119d460efa6be46823026fb
Abdullah Al Mohammad <email.to.titu@gmail.com>
2022-08-01 00:45:48 +0600

    Test code fixed

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test("renders learn react link", () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+  const linkElement = screen.getByText(/Data Analysis for Kids Club/i);
   expect(linkElement).toBeInTheDocument();
 });
